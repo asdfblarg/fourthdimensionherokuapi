6244e24ae58c28f4cca8cebc8d45a9504b1889e2
dzeng <dzeng01@citymail.cuny.edu>
2017-02-28 06:33:29 -0500

    added leftover heroku configs i forgot

--- tutorial/wsgi.py
+++ tutorial/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
 
 application = get_wsgi_application()
+
+
+##### added for heroku
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
