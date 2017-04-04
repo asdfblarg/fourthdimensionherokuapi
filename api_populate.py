9d725bbd73524fbd1ecbe32bdfafe7c04e3ec1f8
dzeng <dzeng01@citymail.cuny.edu>
2017-04-04 05:58:51 -0400

    script has sections randomly to populate tasks and update tasks as complete

--- api_populate.py
+++ api_populate.py
@@ -1,5 +1,5 @@
 import requests
-
+import random
 
 # # GET REQUEST
 # payload = {'designee':'ALL'} # URL QUERY PARAM TEST
@@ -20,3 +20,22 @@ import requests
 # print(r.json())
 
 
+# # POST REQUEST
+# for x in range(15):
+#     label = 'dummy'
+#     rand_person = random.choice(['david','mofi','jun','jabir'])
+#     rand_types = random.choice(['OTHERS','CLEANING','COMPUTER','DELIVERY','TRAVEL'])
+#     # print(rand_person,rand_types)
+#     payload = {'completed':'false', 'label':label, 'description':'dummy post', 'designee': rand_person, 'types': rand_types }
+#     r = requests.post('https://fourthdimensiontestapi.herokuapp.com/tasks/', data=payload)
+#     # print(r)
+#     # print(r.url)
+#     # print(r.json())
+
+# for task_id in range(14, 29):
+#     complete = random.choice([True,False])
+#     if complete:
+#         complete = {'completed':'true', }
+#         r = requests.put('https://fourthdimensiontestapi.herokuapp.com/tasks/{0}/'.format(task_id), data=complete)
+#         # print(r.url)
+#         # print(r.json())
